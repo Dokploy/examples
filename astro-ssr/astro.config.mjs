358ac15e6129cc5efda25e58143baca91c43d13a
Mauricio Siu <47042324+Siumauricio@users.noreply.github.com>
2024-06-30 20:24:44 -0600

    feat: add astro ssr

---
+++ astro-ssr/astro.config.mjs
@@ -0,0 +1,20 @@
+import { defineConfig } from 'astro/config';
+import node from "@astrojs/node";
+
+// https://astro.build/config
+export default defineConfig({
+	output: 'server',
+	site: 'https://example.com',
+	adapter: node({
+		mode: "standalone"
+	  }),
+	server:{
+		port: 3000,
+		host: true
+	},
+	vite:{
+		ssr: {
+			noExternal: ['path-to-regexp'],
+		  },
+	}
+});
