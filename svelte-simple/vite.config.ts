0bb2a0b1e3e18062931ba94c263d10a8c3c76008
Mauricio Siu <47042324+Siumauricio@users.noreply.github.com>
2024-06-30 19:57:48 -0600

    feat: add svelte

---
+++ svelte-simple/vite.config.ts
@@ -0,0 +1,11 @@
+import { defineConfig } from "vite";
+import { svelte } from "@sveltejs/vite-plugin-svelte";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+	plugins: [svelte()],
+	preview: {
+		port: 3000,
+		host: true,
+	},
+});
